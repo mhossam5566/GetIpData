07737c7bda7d22adf38407b475b9994dea6279a6
mhossam5566 <mhossam5566@gmail.com>
2023-06-04 16:13:08 +0300

    updated files

--- app/Http/Controllers/GetController.php
+++ app/Http/Controllers/GetController.php
@@ -11,7 +11,7 @@ class GetController extends Controller
   public function index(Request $request) {
     try {
       $client = new Client();
-      $ipAddress = $request->ip; // عنوان IP غير صحيح
+      $ipAddress = $request->ip;
       $apiKey = '60599fbbdccaca3b8d7fe8006896b499b460aca65b3b6b205ffa8a84';
 
       $url = "https://api.ipdata.co/$ipAddress/?api-key=$apiKey";
@@ -21,24 +21,23 @@ class GetController extends Controller
       $data = $response->getBody()->getContents();
 
       if ($statusCode == 200) {
-        // قم بمعالجة البيانات التي تم استردادها
+       
      $data = json_decode($data);
         return view('welcome', compact('data'));
 
       } else {
-        // قم بالتعامل مع الطلب الغير ناجح وتخزين رسالة الخطأ في متغير
-        $responseData = json_decode($data, true); // تحويل البيانات من صيغة JSON إلى مصفوفة
+        
+        $responseData = json_decode($data, true);
         $errorMessage = $responseData['message'];
         return $errorMessage;
       }
     } catch (ClientException $e) {
-      // تعامل مع الخطأ وإعادة الاستجابة المناسبة
+      
       $response = $e->getResponse();
       $statusCode = $response->getStatusCode();
       $data = $response->getBody()->getContents();
-
-      // قم بتعديل هذا الجزء حسب احتياجاتك
-      $responseData = json_decode($data, true); // تحويل البيانات من صيغة JSON إلى مصفوفة
+      
+      $responseData = json_decode($data, true); 
       $error = $responseData['message'];
       return view('welcome', compact('error'));
     }
